7ad23c0e4d39976d535ff5c2af53308019d93b66
Avas <avas.bajracharya@renegadeinsurance.com>
2024-05-30 09:45:27 +0545

    feat: added linked list append and prepend property

---
+++ src/linkedList.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const newList = new LinkedList(2);
+newList.prepend(4);
+newList.append(5);
+console.log(newList.prepend(9));
