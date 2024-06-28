1120fac886c45460e86840697208080786079c6d
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-28 16:56:29 +0545

    feat: memoize fibonacci function

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,8 +1,34 @@
+let calculations = 0;
+let fastcalc = 0;
+
 function fibonacci(n) {
+  calculations++;
   if (n < 2) {
     return n;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(4));
+function memofibonacci() {
+  let cache = {};
+  return function fib(n) {
+    fastcalc++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      }
+      cache[n] = fib(n - 1) + fib(n - 2);
+      return cache[n];
+    }
+  };
+}
+
+const memofn = memofibonacci();
+fibonacci(4);
+console.log(memofn(4));
+console.log({
+  fastcalc,
+  calculations,
+});
